a4ed853bb7b2555f279dafcb56928f8ca4bbe0d6
izzat <madaminov.izzat.rustambayevich@gmail.com>
2021-01-06 17:54:24 +0500

    Generated ActiveRecord models and queries

--- backend/config/main.php
+++ backend/config/main.php
@@ -42,14 +42,14 @@ return [
                 \yii\bootstrap4\BootstrapAsset::class => false
             ]
         ],
-        /*
+
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
+
     ],
     'params' => $params,
 ];
